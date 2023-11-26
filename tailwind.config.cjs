b3535c63c66db3e1534d97fef20840345802186e
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-11-27 01:23:22 +0330

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,6 @@
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  plugins: [require('flowbite/plugin')],
+  // ... rest of the config
+};
+module.exports = config;
