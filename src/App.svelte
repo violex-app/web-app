a328772c4b044aab005bb73ce88a512aaac48e88
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-12-23 16:02:13 +0330

    init: routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
+  import {Route, Router} from "svelte-routing";
+  import LandingRoute from "./routes/LandingRoute.svelte";
+  import NotFoundRoute from "./routes/NotFoundRoute.svelte";
 
 </script>
 
-<main>
-  Violex App
-</main>
+<div class="dark:bg-gray-800">
+  <Router>
+      <Route path="/" component={LandingRoute}/>
+      <Route path="*" component={NotFoundRoute} />
+  </Router>
+</div>
