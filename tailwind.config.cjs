59a3c316014f594dc59546d527302db2fcc9d573
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-12-23 14:49:10 +0330

    delete: flowbite and tailwind

--- tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-  plugins: [require('flowbite/plugin')],
-  // ... rest of the config
-};
-module.exports = config;
