d9f66660c6799d3a48b9c0075a83dd5b19396f62
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-12-23 16:29:20 +0330

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script>
   import {Route, Router} from "svelte-routing";
   import LandingRoute from "./routes/LandingRoute.svelte";
+  import DashboardRoute from "./routes/DashboardRoute.svelte";
   import NotFoundRoute from "./routes/NotFoundRoute.svelte";
 
 </script>
 
 <div class="dark:bg-gray-800">
   <Router>
-      <Route path="/" component={LandingRoute}/>
-      <Route path="*" component={NotFoundRoute} />
+    <Route path="/" component={LandingRoute}/>
+    <Route path="/dashboard" component={DashboardRoute}/>
+    <Route path="*" component={NotFoundRoute} />
   </Router>
 </div>
